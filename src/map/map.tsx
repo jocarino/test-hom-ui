6829587fc6105be5167b3ec60cbb3a95177bae6d
jocarino <joao.goncalo.veloso@gmail.com>
2023-01-10 19:26:16 +0000

    basic map working

---
+++ src/map/map.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
+import L, { LatLng, LatLngExpression } from 'leaflet';
+
+interface Props {
+}
+
+const MyMap: React.FunctionComponent<Props> = () => {
+    const center: L.LatLngExpression = [51.505, -0.09]
+    const mapRef = useRef<any>({})
+
+
+    const LocationMarker = () => {
+        const [position, setPosition] = useState<L.LatLngExpression | null>(null)
+        const map = useMapEvents({
+            locationfound(e) {
+                setPosition(e.latlng)
+                map.flyTo(e.latlng, map.getZoom())
+            },
+        })
+        useEffect(() => {
+            setTimeout(() => {
+                map.locate()
+                console.log('location')
+            }, 1000);
+
+
+        }, [])
+
+
+        return position === null ? null : (<Marker position={position}>
+            <Popup>You are here</Popup>
+        </Marker>)
+    }
+
+    return (
+        <><h1>Test</h1>
+            <MapContainer ref={mapRef} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <LocationMarker />
+            </MapContainer>
+        </>
+    );
+};
+
+export default MyMap;
---
+++ src/map/mapHomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyMap from './map';
+import MyMap2 from './map copy';
+
+const MapHomePage: React.FC = () => {
+    return (
+        <MyMap />
+    );
+};
+
+export default MapHomePage;
