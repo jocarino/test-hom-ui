f7af7648bb3c544c34612de9e2944785992f5842
jocarino <joao.goncalo.veloso@gmail.com>
2023-01-14 18:31:10 +0000

    center button and locaiton tracking

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
+import React, { useState, useEffect, useMemo } from 'react';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 
 interface Props {
@@ -7,41 +7,64 @@ interface Props {
 
 const MyMap: React.FunctionComponent<Props> = () => {
     const center: L.LatLngExpression = [51.505, -0.09]
-    const mapRef = useRef<any>({})
+    const defaultZoom = 13
+    const [map, setMap] = useState<L.Map | null>(null)
+    const [marker, setMarker] = useState(null)
+    const [location, setLocation] = useState({ lat: 0, lng: 0 });
+    const [error, setError] = useState<GeolocationPositionError | null>(null);
 
-
-    const LocationMarker = () => {
-        const [position, setPosition] = useState<L.LatLngExpression | null>(null)
-        const map = useMapEvents({
-            locationfound(e) {
-                setPosition(e.latlng)
-                map.flyTo(e.latlng, map.getZoom())
+    useEffect(() => {
+        const watchId = navigator.geolocation.watchPosition(
+            (position) => {
+                setLocation({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                });
+                if (map) {
+                    map.setView([location.lat, location.lng])
+                }
             },
-        })
-        useEffect(() => {
-            setTimeout(() => {
-                map.locate()
-                console.log('location')
-            }, 1000);
+            (error) => () => {
+                setError(error)
+                console.log(error);
+            }
+        );
+        console.log(`location ${location.lat},${location.lng}`);
 
+        return () => navigator.geolocation.clearWatch(watchId);
+    }, [location.lat, location.lng])
 
-        }, [map])
-
-
-        return position === null ? null : (<Marker position={position}>
-            <Popup>You are here</Popup>
-        </Marker>)
+    const reCenterButton = (map: L.Map) => {
+        const onClick = () => {
+            map.setView([location.lat, location.lng], defaultZoom)
+        }
+        return (
+            <p>
+                <button onClick={onClick}>Re-center</button>
+            </p >
+        )
     }
 
-    return (
-        <><h1>Test</h1>
-            <MapContainer ref={mapRef} center={center} zoom={13} scrollWheelZoom={false}>
+    const LocationMarker = () =>
+        <Marker position={location} />
+
+
+    const displayMap = useMemo(
+        () => (
+            <MapContainer ref={setMap} center={center} zoom={defaultZoom} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <LocationMarker />
             </MapContainer>
+        ), [location]
+    )
+
+    return (
+        <><h1>Test</h1>
+            {displayMap}
+            {map ? reCenterButton(map) : null}
         </>
     );
 };
