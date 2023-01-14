3bc374a700948d411def91f4209c6eba6dbbb9bd
jocarino <joao.goncalo.veloso@gmail.com>
2023-01-14 16:05:30 +0000

    clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import arrow from './arrow-symbol.png';
 import './App.css';
 import MapHomePage from './map/mapHomePage';
-import { Helmet } from 'react-helmet';
 import 'leaflet/dist/leaflet.css'
 
 function App() {
   return (
     <>
-      {/* <Helmet>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </Helmet> */}
       <MapHomePage />
     </>
   );
