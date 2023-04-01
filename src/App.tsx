f92e729365414355465155bb860d5417ca1a7c02
jocarino <joao.goncalo.veloso@gmail.com>
2023-04-01 21:11:34 +0100

    fix: have the feed come from the db and not from storage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import 'leaflet/dist/leaflet.css'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import UploadPosterLocation from './map/uploadPosterLocation';
 import PosterFeed from './poster/posterFeed';
+import PosterCMSPage from './poster/admin/updatePoster';
+import CreatePosterPage from './poster/admin/createPoster';
 
 function App() {
   return (
@@ -15,6 +17,10 @@ function App() {
           <Route path="/posters"
             element={<PosterFeed />} />
           <Route path='/upload/poster-location' element={<UploadPosterLocation />}></Route>
+          <Route path="/admin/poster"
+            element={<PosterCMSPage />} />
+          <Route path="/admin/poster/new"
+            element={<CreatePosterPage />} />
         </Routes>
       </BrowserRouter>
     </>
