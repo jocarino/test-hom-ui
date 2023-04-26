03e30fd47279f54ef4ba06401fe8c1d81aaf3b9d
goncalodasilva <gnunosilva@hotmail.com>
2023-04-26 08:55:12 +0200

    more beautiful header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,10 +1,21 @@
+import { Button } from "@mui/material";
 import React from "react";
 import { Link, redirect } from "react-router-dom";
 import { actionTypes } from "../context/reducer";
 import { useStateValue } from "../context/StateProvider";
+import "./Header.css";
+import styled from 'styled-components';
+import { sign } from "crypto";
+import ImageComponent from "../common/imageComponent";
+import HOM_UI from '../common/images/hom-logo-bw.png'
 
 function Header() {
     const [{ user }, dispatch] = useStateValue();
+
+    const SignInStyledButton = styled(Button)`
+        
+        color: #000000 !important;
+    `;
 
     const signOut = () => {
         dispatch({
@@ -15,17 +26,28 @@ function Header() {
     }
 
     return (
-        <>
-            {!user ? (
-                <Link to="/login">sign in</Link>
-            ) : (
-                <>
-                    {user.displayName}
-                    <span onClick={signOut}>sign out</span>
-                </>
-            )
-            }
-        </>
+        <div id="header-root">
+            <div />
+            <div>
+                <ImageComponent
+                    id={`imgcheckmark_HOM_UI`}
+                    image={HOM_UI}
+                    altText="HOM_UI logo"
+                    size={{ width: 50, height: 50 }}
+                />
+            </div>
+            <div>
+                {!user ? (
+                    <Link to="/login">
+                        <SignInStyledButton variant="outlined" size="large">Sign In</SignInStyledButton>
+                    </Link>
+                ) : (
+                    <SignInStyledButton variant="outlined" size="large" onClick={signOut}>Sign Out</SignInStyledButton>
+
+                )
+                }
+            </div>
+        </div>
     )
 }
 
