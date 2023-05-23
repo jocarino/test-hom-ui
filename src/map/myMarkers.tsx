fa30945374925aa0797fb25495f30a90c52b9f75
jocarino <joao.goncalo.veloso@gmail.com>
2023-05-23 19:55:36 +0100

    feat: have title and description in the popup marker

--- src/map/myMarkers.tsx
+++ src/map/myMarkers.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Marker, useMapEvents } from "react-leaflet";
+import { Marker, Popup, useMapEvents } from "react-leaflet";
 import { PosterDoc } from "../types/poster";
 import L, { Icon } from "leaflet";
 import icon from 'leaflet/dist/images/marker-icon.png'
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png'
 import redMarker from '../common/images/location-pin.png'
+import styled from "styled-components";
 
 type MyMarkersProps = {
     location: {
@@ -15,6 +16,23 @@ type MyMarkersProps = {
     posters: PosterDoc[];
     loadPosters: () => void;
 }
+
+const PosterPopupDescription = styled.p`
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;  
+    overflow: hidden;
+`
+const PosterPopupTitle = styled.p`
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;  
+    overflow: hidden;
+`
+
+const PosterPopup = styled(Popup)`
+    max-width: 200px;
+`
 
 const MyMarkers: React.FunctionComponent<MyMarkersProps> = (props: MyMarkersProps) => {
     const { location, posters, loadPosters } = props;
@@ -52,10 +70,17 @@ const MyMarkers: React.FunctionComponent<MyMarkersProps> = (props: MyMarkersProp
             <Marker position={location} icon={LocationIcon} />
             {posters.length > 0 ?
                 posters.filter((poster) => poster.coordinates && poster.coordinates != null)
-                    .map((poster) => <Marker
-                        position={[poster.coordinates!.latitude, poster.coordinates!.longitude]}
-                        key={poster.id} icon={PosterIcon} />) :
-                undefined}
+                    .map((poster) =>
+                        <Marker
+                            position={[poster.coordinates!.latitude, poster.coordinates!.longitude]}
+                            key={poster.id} icon={PosterIcon}>
+                            <PosterPopup >
+                                <PosterPopupTitle>{poster.title}</PosterPopupTitle>
+                                <PosterPopupDescription >{poster.description}</PosterPopupDescription>
+                            </PosterPopup>
+                        </Marker >) :
+                undefined
+            }
         </>
     );
 };
